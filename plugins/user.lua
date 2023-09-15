89aaea8849cf5e20feccfa57a45edfc852fcdd04
Jonathan Frankel <github@habanerojam.xyz>
2023-09-15 17:23:20 +0800

    some better plugins

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,22 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+        require("nvim-surround").setup({
+            -- Configuration here, or leave empty to use defaults
+        })
+    end
+  },
+
+  {
+    "AstroNvim/astrocommunity",
+    { import = "astrocommunity.colorscheme.dracula-nvim" },
+    { import = "astrocommunity.editing-support.todo-comments-nvim" },
+    { import = "astrocommunity.motion.mini-surround" },
+    { import = "astrocommunity.pack.rust" },
+  },
 }
